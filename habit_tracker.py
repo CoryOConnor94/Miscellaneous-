fa2fa418270e273a63743428f687cc668dca152a
CoryOConnor94 <coryoconnor1994@gmail.com>
2025-02-13 12:21:11 +0000

    Restructured code into functions

--- habit_tracker.py
+++ habit_tracker.py
@@ -1,44 +1,62 @@
 import requests
 from datetime import datetime
 
-USERNAME = ''
-TOKEN = ''
-GRAPH_ID = "graph1"
-pixela_endpoint = 'https://pixe.la/v1/users'
-today = datetime.today()
-print(today)
-
-params = {
-    "token":TOKEN,
-    "username":USERNAME,
-    "agreeTermsOfService":"yes",
-    "notMinor":"yes",
-}
-
-# response = requests.post(url=pixela_endpoint, json=params)
-# print(response.text)
-
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
+USERNAME = 'fitmitch1'      # Validation rule: [a-z][a-z0-9-]{1,32}
+TOKEN = ''      # Validation rule: [ -~]{8,128}
 headers = {
-    'X-USER-TOKEN':TOKEN
+    'X-USER-TOKEN': TOKEN
 }
+GRAPH_ID = "graph1"     # Validation rule: ^[a-z][a-z0-9-]{1,16}
+pixela_endpoint = 'https://pixe.la/v1/users'
+today = datetime.today().strftime('%Y%m%d')
 
-# graph_config = {
-#     "id":"graph1",
-#     "name":"Jogging Graph",
-#     "unit":"Km",
-#     "type":"float",
-#     "color":"shibafu",
-# }
 
-# requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+def create_user():
+    """Creates pixela user with user created token and username"""
+    params = {
+        "token": TOKEN,
+        "username": USERNAME,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes",
+    }
 
-pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
-pixel_data = {
-    "date":today.strftime('%Y%m%d'),
-    "quantity": input("How many Km did you run today? "),
-}
+    response = requests.post(url=pixela_endpoint, json=params)
+    print(response.text)
 
-response = requests.post(url=pixel_endpoint, json=pixel_data, headers=headers)
-print(response.text)
+
+def create_graph():
+    """Creates pixela graph"""
+    graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+    graph_config = {
+        "id": "graph1",
+        "name": "Jogging Graph",
+        "unit": "Km",
+        "type": "float",
+        "color": "shibafu",
+    }
+
+    requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+
+def add_pixel():
+    """Adds pixel habit entry to graph"""
+    pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+    pixel_data = {
+        "date": today,
+        "quantity": input("How many Km did you run today? "),
+    }
+
+    response = requests.post(url=pixel_endpoint, json=pixel_data, headers=headers)
+    print(response.text)
+
+
+def main():
+    """Main flow of program"""
+    # create_user()     # Uncomment to create new user
+    # create_graph()    # Uncomment to create new graph
+    add_pixel()
+
+
+if __name__=='__main__':
+    main()
--- workout_tracker.py
+++ workout_tracker.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 APP_ID = ''
 API_KEY = ''
+SHEETY_BEARER = ''
 
 GENDER = ''
 WEIGHT_KG = ''
@@ -14,39 +15,58 @@ AGE = ''
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 sheety_endpoint = ''
 
-now = datetime.now()
-date = now.strftime('%x')
-time = now.strftime('%X')
-exercise_text = input("Tell me which exercises you did: ")
 
-headers = {
-    'x-app-id': APP_ID,
-    'x-app-key': API_KEY,
-}
-
-params = {
-    'query': exercise_text,
-    'gender': GENDER,
-    'weight_kg': WEIGHT_KG,
-    'height_cm': HEIGHT_CM,
-    'age': AGE,
-}
-
-response = requests.post(url=exercise_endpoint, json=params, headers=headers)
-result = response.json()
+def get_date_and_time():
+    """Returns current date and time formatted"""
+    now = datetime.now()
+    return now.strftime('%d/%m/%Y'), now.strftime('%X')
 
 
-for exercise in result["exercises"]:
-    sheet_inputs = {
-        "sheet1": {
-            "date": date,
-            "time": time,
-            "exercise": exercise["name"].title(),
-            "duration": exercise["duration_min"],
-            "calories": exercise["nf_calories"]
-        }
+def get_exercise_data():
+    """Retrieves workout data from Nutritionix API"""
+    exercise_text = input("Tell me which exercises you did: ")
+    headers = {
+        'x-app-id': APP_ID,
+        'x-app-key': API_KEY,
+    }
+    params = {
+        'query': exercise_text,
+        'gender': GENDER,
+        'weight_kg': WEIGHT_KG,
+        'height_cm': HEIGHT_CM,
+        'age': AGE,
     }
 
-    sheet_response = requests.post(sheety_endpoint, json=sheet_inputs)
+    response = requests.post(url=exercise_endpoint, json=params, headers=headers)
+    return response.json()
 
-    print(sheet_response.text)
+
+def log_exercise_to_sheety(exercises, date, time):
+    """Logs exercise data to google sheet through sheety API"""
+    bearer_headers = {
+        'Authorization': f'Bearer {SHEETY_BEARER}'
+    }
+    for exercise in exercises:
+        sheet_inputs = {
+            "sheet1": {
+                "date": date,
+                "time": time,
+                "exercise": exercise["name"].title(),
+                "duration": exercise["duration_min"],
+                "calories": exercise["nf_calories"]
+            }
+        }
+
+        sheet_response = requests.post(sheety_endpoint, json=sheet_inputs, headers=bearer_headers)
+        print(sheet_response.text)
+
+
+def main():
+    """Main flow of program"""
+    date, time = get_date_and_time()
+    exercise_data = get_exercise_data()
+    log_exercise_to_sheety(exercise_data['exercises'], date, time)
+
+
+if __name__ == '__main__':
+    main()
